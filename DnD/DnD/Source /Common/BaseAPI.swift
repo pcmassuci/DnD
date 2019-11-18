61851b5e3a62ea62824ed72e6f30c8bd80b9aeca
Paulo César Morandi Massuci <pmassuci@ciandt.com>
2019-11-18 10:22:41 -0300

    commit inicial

---
+++ DnD/DnD/Source /Common/BaseAPI.swift
@@ -0,0 +1,10 @@
+//
+//  BaseAPI.swift
+//  DnD
+//
+//  Created by Paulo Cesar Morandi Massuci on 18/11/19.
+//  Copyright © 2019 Paulo Cesar Morandi Massuci. All rights reserved.
+//
+
+import Foundation
+
